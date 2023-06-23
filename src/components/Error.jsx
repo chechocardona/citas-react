57b4883c046203193e81ff798a99dd8d6d1f248c
Sergio <chechocardona@gmail.com>
2023-06-23 17:04:48 -0500

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({children}) => {
+  return (
+    <div className="bg-red-800 text-white uppercase text-center p-3 font-bold m-3 rounded-md">
+        {children}
+    </div>
+  )
+}
+
+export default Error
